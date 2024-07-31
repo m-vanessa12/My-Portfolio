f8d755e5f563f0183bc08b58a29888d460faa934
unknown <v.mukamanzi@alustudent.com>
2024-07-31 10:06:59 +0200

    I have added the menu-bar hamburger

--- src/menu-bar/menuBar.js
+++ src/menu-bar/menuBar.js
@@ -1,6 +1,7 @@
 import './menu.css';
 import { HashLink as Link } from 'react-router-hash-link';
 import React, { useState } from 'react';
+import { FaBars, FaTimes } from 'react-icons/fa'; 
 
 const MenuBar = () => {
 
@@ -20,17 +21,17 @@ const MenuBar = () => {
             </div>
 
             <div className="menu-icon" onClick={toogleMenu}>
-                {menuOpen ? <i className='fas fa-times'></i> : <i className='fas fa-bars' ></i>}
+            {menuOpen ? <FaTimes /> : <FaBars />}
             </div>
             <div className={`menu ${menuOpen ? 'show' : ''}`}>
                 
                 <ul>
-                    <li><Link to='#home' smooth onClick={() => setMenuOpen(false)}><a href="#Home">Home</a></Link></li>
-                    <li><Link to='#about' smooth onClick={() => setMenuOpen(false)}><a href="#about">About</a></Link></li>
-                    <li><Link to='#skills' smooth onClick={() => setMenuOpen(false)}><a href="#skills">Skills</a></Link></li>
-                    <li><Link to='#project' smooth onClick={() => setMenuOpen(false)}><a href="#project">Project</a></Link></li>
-                    <li><Link to='#experience' smooth onClick={() => setMenuOpen(false)}><a href="#experience">Experience</a></Link></li>        
-                    <li><Link to='#contact' smooth onClick={() => setMenuOpen(false)}><a href="#contact">Contact</a></Link></li>
+                    <li><Link to='#home' smooth onClick={() => setMenuOpen(false)}>Home</Link></li>
+                    <li><Link to='#about' smooth onClick={() => setMenuOpen(false)}>About</Link></li>
+                    <li><Link to='#skills' smooth onClick={() => setMenuOpen(false)}>Skills</Link></li>
+                    <li><Link to='#project' smooth onClick={() => setMenuOpen(false)}>Project</Link></li>
+                    <li><Link to='#experience' smooth onClick={() => setMenuOpen(false)}>Experience</Link></li>        
+                    <li><Link to='#contact' smooth onClick={() => setMenuOpen(false)}>Contact</Link></li>
                     {/* <li> <a href="https://drive.google.com/file/d/1qM8Q4Zpn01gljaRhVR27ogXbmk-UBZ6L/view?usp=sharing/view?usp=sharing" target="_blank">
                         <button className='resume'>Resume</button>
                     </a> </li> */}
