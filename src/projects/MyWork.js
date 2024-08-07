0785151c6b691e43009a345a365f270b0185caa0
unknown <v.mukamanzi@alustudent.com>
2024-08-07 21:51:23 +0200

    I added the mentorship in the projects in my projects

--- src/projects/MyWork.js
+++ src/projects/MyWork.js
@@ -1,6 +1,7 @@
 import '../projects/work.css';
 import house from '../img/house_keeping.png';
-import diabites_predictor from '../img/diabete_predictor.png'
+import diabites_predictor from '../img/diabete_predictor.png';
+import mentorship from '../img/mentor_me.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faExternalLinkAlt} from '@fortawesome/free-solid-svg-icons';
 
@@ -58,6 +59,30 @@ const Projects = () => {
                 <div className="work-done2">
                     <img src={diabites_predictor} alt="house keeping app" />
                 </div>        
+
+            </div>
+
+
+            <div className="work-display">
+                <div className="work-done">
+                    <img src={mentorship} alt="house keeping app" />
+                </div>
+                <div className="work-description">
+                    <div className="project-titles">House Keeping App</div>
+                    <div className="project-subtitle">UX/UI Project</div>
+                    <div className="description">The Housekeeping and Laundry Booking App is a user-friendly platform that simplifies booking professional 
+                        housekeeping and laundry services, whether for one-time deep cleaning, regular house maintenance, or laundry needs.</div>
+                        <div className="features">
+                            <span className='feature-title'>Features:</span>
+                            <span className='features-list'>Laundry Booking, House Keeping Services</span>
+                        </div>
+                    <div className="moree">
+                        <a href="https://www.figma.com/design/txsi0ILYuidP3obOfFLRUl/Women-Mentorship-Platform?node-id=0-1&t=Ap3HTwZ6r5bbty9S-1" target='_blank'>
+                        <span>Full project</span>
+                        <FontAwesomeIcon icon={faExternalLinkAlt} style={{ width: '18px', height: '25px', color: '#FFF', marginLeft : '15px', marginTop: '5px'}} />
+                        </a>
+                        </div>
+                  </div>          
 
             </div>
 
